a192a769665dc43531bb59358b6dc718c4a6224b
Christian Larsen <christian.p.larsen.94@gmail.com>
2021-06-04 14:11:50 -0500

    loop practice

--- js/loop_classwork.js
+++ js/loop_classwork.js
@@ -1,15 +1,30 @@
 //whacky fun time with loops
 
-var message = '';
+//var message = '';
 
-var howMany = prompt('how many?');
+//var howMany = prompt('how many?');
 
-for (let index = 0; index <= howMany; index++) {
-    message = message + index + '\n';
+//for (let index = 0; index <= howMany; index++) {
+//   message = message + index + '\n';
 
+//}
+
+//alert(message);
+
+//alert(index);
+//line 12 won't do anything because I put let index inside the loop
+
+for (var index = 0; index < 10; index++)
+{
+    //do something
+    alert("index: " + index + '\n');
 }
 
-alert(message);
+var index = 0
+//line 23 resets our variable to zero so the next loop actiates again
 
-alert(index);
-//line 12 won't do anything because I put let index inside the loop
+while(index < 10) {
+    //do something
+    alert("index: " + index + '\n');
+    index++;
+}
