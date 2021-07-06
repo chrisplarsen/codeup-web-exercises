02b36a66c2618e245a1051aecd661c618105cab7
Christian Larsen <christian.p.larsen.94@gmail.com>
2021-07-06 10:54:48 -0500

    tuesday morning arm up

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -16,3 +16,26 @@ countSpaces("idek if this is correct");
 countSpaces(0);
 countSpaces(NaN);
 countSpaces();
+
+// July 6th 2021
+// TODO: Create a function named capitalizeAllNames that accepts an array of strings and returns the array with all of its elements capitalized.
+// Example:  capitalizeAllNames(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["BOB","SETH","TOFU","TOYOTA"]
+function capitalizeAllNames(index) {
+    //return index.toUpperCase()
+    var allCapsArray = []
+}
+
+capitalizeAllNames(["bob", "Seth", "TOFU", "Toyota"]);
+// TODO: Create a function named capitalizeFirstLetter that accepts an array of strings and returns the array with all of the first letters in
+//  each string being capitalized while having the rest of the string lowercase look at the example below for reference.
+// Example:  capitalizeFirstLetter(["bob","Seth","TOFU","Toyota"]) ---- returns ----> ["Bob","Seth","Tofu","Toyota"]
+function capitalizeFirstLetter(index2) {
+    index2.split(",");
+    for (var i = 0; i < index2.length; i++) {
+        index2[i] = index2[i].charAt(0).toUpperCase() + index2[i].slice(1);
+
+    }
+    index2.join(",")
+}
+
+capitalizeFirstLetter(["bob", "Seth", "TOFU", "Toyota"]);
