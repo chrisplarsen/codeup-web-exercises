9267a5bec25a9ebb469ea1cd23577e3bf3042044
Christian Larsen <christian.p.larsen.94@gmail.com>
2021-07-08 09:28:38 -0500

    morning exercises 08JUL2021

--- js/morningWarmUps.js
+++ js/morningWarmUps.js
@@ -74,9 +74,59 @@ var personThree = {
     firstName: "arlene",
     lastName: "martinez",
     ageInYears: 25,
-    heightInInches: 62
+    heightInInches: 62,
+    personThreeIntro: function () {
+        return "Hello, My name is " + this.firstName + " " + pthis.lastName + " and I am " + this.age
+            + " years old.";
+    }
 }
 
-function bla() {
-    return "Hello, My name is " + personThree.firstName + " " + personThree.lastName + " and I am " + personThree.age + " years old.";
+// July 8th 2021
+// Part 1
+// TODO: Create a function that takes an array of strings and returns an array of objects
+//  where each object has 2 properties: originalString and countOfLetters
+// Example: input: ["hello", "dave"]
+//          returns: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                   ]
+
+function arraysToObjects(index) {
+    var notSureOnName = {}
+        notSureOnName.originalString = index;
+        notSureOnName.countOfLetters = index.length;
 }
+console.log(arraysToObjects);
+
+// Part 2
+// TODO: Create a function that takes the array of objects from PART 1
+//  and returns a string of all of the originalStrings concatenated with spaces in between
+// Example: input: [
+//                    { "originalString":"hello", lengthOfOriginalString:5 },
+//                    { "originalString":"dave", lengthOfOriginalString:4 }
+//                  ]
+//          returns: "hello dave"
+
+function arrayToConString (index) {
+    return object.originalString
+}
+// TODO: Create a function named getTallUsers that accepts an array of objects. The objects in the array will be in the same format
+//  as PersonOne, PersonTwo, and PersonThree. The functions job is to return an array of users that have heightInInches equal to or greater than 65.
+var people = [personOne,personTwo,personThree];
+// Example: getTallUsers(people)
+// ---- returns ----> [{firstName: "silvia", lastName: "floopertan", ageInYears: 34, heightInInches: 65},
+//
+
+function getTallUsers (index) {
+    for (i = 0; i <= index.length - 1; i++);
+        if (index.heightInInches >= 65) {
+            return index;
+        }
+}
+
+console.log(getTallUsers);
+
+
+
+
+
